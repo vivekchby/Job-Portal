3eabb9bc9f6d76f765f8137fcaaa7b76cc5998e2
vivekchby <mr.vivekchaubey5@gmail.com>
2025-11-08 20:36:18 +0530

    done

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     mongoose.connection.on('connected',() => console.log('Database Connected'));
   
-    await mongoose.connect(`${process.env.MONGODB_URI}/fitnessvivek`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/Fitnessvivek`);
 }
 
 export default connectDB
